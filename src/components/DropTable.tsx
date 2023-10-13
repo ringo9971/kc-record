4d8e73a872f3d124548c745ec5834ba9e72a699c
ringo9971 <ab7usa@gmail.com>
2023-10-14 00:17:02 +0900

    :lipstick: 時間の表示を調整

--- src/components/DropTable.tsx
+++ src/components/DropTable.tsx
@@ -30,16 +30,25 @@ export const DropTable = (props: DropsItemConfig | undefined): JSX.Element => {
             </TableRow>
           </TableHead>
           <TableBody>
-            {props?.items?.map((drop) => (
-              <TableRow key={drop.id}>
-                <TableCell>{drop.event}</TableCell>
-                <TableCell>{drop.area}</TableCell>
-                <TableCell>{drop.outcome}</TableCell>
-                <TableCell>{drop.ship}</TableCell>
-                <TableCell>{drop.comment}</TableCell>
-                <TableCell>{drop.time}</TableCell>
-              </TableRow>
-            ))}
+            {props?.items?.map((drop) => {
+              const time = new Date(drop.time).toLocaleString('jp-JP', {
+                timeZone: 'Asia/Tokyo',
+                month: '2-digit',
+                day: '2-digit',
+                hour: '2-digit',
+                minute: '2-digit',
+              });
+              return (
+                <TableRow key={drop.id}>
+                  <TableCell>{drop.event}</TableCell>
+                  <TableCell>{drop.area}</TableCell>
+                  <TableCell>{drop.outcome}</TableCell>
+                  <TableCell>{drop.ship}</TableCell>
+                  <TableCell>{drop.comment}</TableCell>
+                  <TableCell>{time}</TableCell>
+                </TableRow>
+              );
+            })}
           </TableBody>
         </Table>
       </TableContainer>
