db9bdae7ed4e7710e505eab66e0a3bc37cbd41ea
ringo9971 <ab7usa@gmail.com>
2023-10-15 23:39:51 +0900

    :sparkles: drop 情報を Firestore から更新できるように

---
+++ src/api/firebaseUpdateDrop.ts
@@ -0,0 +1,71 @@
+import { User } from 'firebase/auth';
+import { doc, setDoc, Firestore } from 'firebase/firestore';
+
+import { deleteDrop } from './firebaseDeleteDrop';
+import { getAreaFirestoreDrops } from './firebaseGetDrops';
+import { Drop, FirestoreDrop } from './types';
+
+export const updateDrop = async (
+  user: User | null,
+  firestore: Firestore,
+  dropId: string,
+  preDrop: Drop,
+  updateDrop: Drop
+) => {
+  if (!user) return null;
+
+  if (preDrop.event === updateDrop.event && preDrop.area === updateDrop.area) {
+    const drops = await getAreaFirestoreDrops(
+      user,
+      firestore,
+      updateDrop.event,
+      updateDrop.area
+    );
+    const firestoreDrops: FirestoreDrop[] = drops.map((drop) => {
+      return {
+        ...drop,
+        outcome: drop.id === dropId ? updateDrop.outcome : drop.outcome,
+        ship: drop.id === dropId ? updateDrop.ship : drop.ship,
+        comment: drop.id === dropId ? updateDrop.comment : drop.comment,
+      };
+    });
+
+    await setDoc(
+      doc(firestore, 'drops', user.uid, updateDrop.event, updateDrop.area),
+      {
+        results: firestoreDrops,
+      }
+    );
+    return;
+  }
+
+  await deleteDrop(user, firestore, dropId, preDrop.event, preDrop.area);
+  await _createDrop(user, firestore, updateDrop);
+};
+
+const _createDrop = async (
+  user: User | null,
+  firestore: Firestore,
+  drop: Drop
+) => {
+  if (!user) return null;
+
+  const firestoreDrops = await getAreaFirestoreDrops(
+    user,
+    firestore,
+    drop.event,
+    drop.area
+  );
+
+  firestoreDrops.push({
+    id: drop.id,
+    time: drop.time,
+    outcome: drop.outcome,
+    ship: drop.ship,
+    comment: drop.comment,
+  });
+
+  await setDoc(doc(firestore, 'drops', user.uid, drop.event, drop.area), {
+    results: firestoreDrops,
+  });
+};
