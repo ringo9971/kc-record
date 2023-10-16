a1c720b6473af9931c0803bed37e55cd24fbba7e
ringo9971 <ab7usa@gmail.com>
2023-10-16 21:22:51 +0900

    :sparkles: Topbar に Menu を追加

--- src/TopBar.tsx
+++ src/TopBar.tsx
@@ -1,11 +1,34 @@
-import { AppBar, Box, Button, Toolbar } from '@mui/material';
-import { memo } from 'react';
+import {
+  AppBar,
+  Avatar,
+  Box,
+  Button,
+  Divider,
+  IconButton,
+  ListItemIcon,
+  Menu,
+  MenuItem,
+  Toolbar,
+  Tooltip,
+} from '@mui/material';
+import { memo, useState, MouseEvent } from 'react';
 import { Link } from 'react-router-dom';
 
 import { useUser } from './hooks/useUser';
+import { Logout, PersonAdd, Person } from '@mui/icons-material';
 
 const TopBar = (): JSX.Element => {
   const { user } = useUser();
+
+  const [anchorEl, setAnchorEl] = useState<HTMLElement | null>(null);
+  const open = Boolean(anchorEl);
+
+  const handleClick = (event: MouseEvent<HTMLElement>) => {
+    setAnchorEl(event.currentTarget);
+  };
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
 
   return (
     <Box mb={3}>
@@ -13,7 +36,42 @@ const TopBar = (): JSX.Element => {
         <Toolbar>
           <Box flexGrow="1">ドロップ記録</Box>
           {user ? (
-            'ログイン済み'
+            <Box>
+              <Box>
+                <Tooltip title="アカウント設定">
+                  <IconButton onClick={handleClick}>
+                    <Avatar />
+                  </IconButton>
+                </Tooltip>
+              </Box>
+              <Menu
+                anchorEl={anchorEl}
+                open={open}
+                onClose={handleClose}
+                onClick={handleClose}
+              >
+                <MenuItem onClick={handleClose}>
+                  <ListItemIcon>
+                    <Person />
+                  </ListItemIcon>
+                  プロフィール
+                </MenuItem>
+                <Divider />
+                <MenuItem onClick={handleClose}>
+                  <ListItemIcon>
+                    <PersonAdd />
+                  </ListItemIcon>
+                  フレンド追加
+                </MenuItem>
+                <Divider />
+                <MenuItem onClick={handleClose}>
+                  <ListItemIcon>
+                    <Logout />
+                  </ListItemIcon>
+                  ログアウト
+                </MenuItem>
+              </Menu>
+            </Box>
           ) : (
             <Button component={Link} to="/login" color="inherit">
               ログインはこちらから
