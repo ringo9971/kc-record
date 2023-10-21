155c1979a2e07ad2bb5ef42b98952d17b2363a32
ringo9971 <ab7usa@gmail.com>
2023-10-21 11:15:03 +0900

    :sparkles: ドロップ表示画面で色をつけるように

--- src/components/ShipInfo.tsx
+++ src/components/ShipInfo.tsx
@@ -1,7 +1,14 @@
+import { Box } from '@mui/material';
+import { useRareContext } from '../lib/RareContext';
+
 interface ShipInfoProps {
   ship?: string;
 }
 
-export const ShipInfo = (props: ShipInfoProps) => {
-  return <>{props.ship}</>;
+export const ShipInfo = ({ ship }: ShipInfoProps) => {
+  const { rareDrops } = useRareContext();
+
+  const color = ship ? rareDrops.get(ship) ?? 'balck' : 'black';
+
+  return <Box sx={{ color }}>{ship}</Box>;
 };
